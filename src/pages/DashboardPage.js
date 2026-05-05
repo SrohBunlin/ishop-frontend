6c2702e531f7eb066915a1b53d21adf018477d64
Sroh Bunlin <srohbunlin@Srohs-MacBook-Pro.local>
2026-05-05 10:14:41 +0700

    new update

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -316,7 +316,7 @@ const DashboardPage = () => {
     const npage = Math.ceil(filteredOrders.length / recordsPerPage); // ចំនួនទំព័រសរុប
     const numbers = [...Array(npage + 1).keys()].slice(1); // បង្កើតលេខ ១, ២, ៣...
     return (
-        <div className="d-flex container-fluid">
+        <div className="flex container-fluid">
 
             {/* 2. MAIN CONTENT AREA */}
             <div className="d-flex align-items-start">
